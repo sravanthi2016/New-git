324f8c2e8634f47846ef882ba60df6458fe76c3d
sravanthi2016 <137877049+sravanthi2016@users.noreply.github.com>
2023-08-14 14:57:03 +0530

    Update phone.sh

--- phone.sh
+++ phone.sh
@@ -1,6 +1,6 @@
 KULA, HAWAII - AUGUST 12: Brook Cretton holds up bowls that he found while looking through the rubble of a home that was destroyed by wildfire on August 12, 2023 in Kula, Hawaii. At least 80 people were killed and thousands were displaced after a wind-driven wildfire devastated the towns of Lahaina and Kula on Tuesday. Crews are continuing to search for missing people. Justin Sullivan/Getty Images/AFP (Photo by JUSTIN SULLIVAN / GETTY IMAGES NORTH AMERICA / Getty Images via AFP) (Getty Images via AFP)
 Hawaii wildfires are the deadliest wildfire in the US since the 2018 Camp Fire in California that killed at least 85 people and destroyed the town of Paradise. The fire went from zero to 100 and essentially managed to "eat an entire town in four hours".
-
+git commands pull requests reasy
 Here are 10 points you should know Hawaiicreated script for testing git commands
 practicing git commands
 re1) Hawaii Governor Josh Green has said that the number of confirmed deaths from Maui wildfires has gone up to 89, making it the deadliest wildfire in more than 100 years in the US. 
