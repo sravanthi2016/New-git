6d37d591412cadb8dde75645e5fd3976425c9952
sravanthi2016 <137877049+sravanthi2016@users.noreply.github.com>
2023-08-14 11:11:57 +0530

    Update phone.sh

--- phone.sh
+++ phone.sh
@@ -3,4 +3,20 @@ Hawaii wildfires are the deadliest wildfire in the US since the 2018 Camp Fire i
 
 Here are 10 points you should know Hawaiicreated script for testing git commands
 practicing git commands
-removing lines
+re1) Hawaii Governor Josh Green has said that the number of confirmed deaths from Maui wildfires has gone up to 89, making it the deadliest wildfire in more than 100 years in the US. 
+
+2) The Governor added that the death toll is further expected to go up and some victims were positively identified on Saturday. He noted that around 2,200 structures have been destroyed or damaged in West Maui with 86 percent of them being residential buildings. 
+
+3) Governor Green said that officials will review policies and procedures for improving safety. He said, “People have asked why we are reviewing what’s going on and it’s because the world has changed. A storm now can be a hurricane-fire or a fire-hurricane…That’s what we experienced, that’s why we’re looking into these policies, to find out how we can best protect our people."
+
+4) US National Weather Service has said that Hurricane Dora was partly to be blamed for the strong winds that drove the flames initially leading to power being knocked out and firefighting helicopters getting grounded, reported AP. 
+
+5) Hawaii Tourism Authority has said that around 46,000 residents and visitors have flown out of West Maui's Kahului Airport since the start of devastation in the historic resort city of Lahaina. 
+
+6) “In the weeks ahead, the collective resources and attention of the federal, state and county government, the West Maui community, and the travel industry must be focused on the recovery of residents who were forced to evacuate their homes and businesses," Hawaii Tourism Authority said in a statement quoted by AP. 
+
+7) As per Maui County estimates, over 80 percent of the structures in Lahaina were damaged or destroyed and around 4,500 residents are in need of shelter. 
+
+8) Hawaii wants to work with Airbnb to ensure that rental homes are available for locals. The  Governor hopes that Airbnb can provide homes with three to nine-month rentals available. 
+
+9) Hawaii Senator Mazie Hirono while speaking at CNN's ‘State of Union’ on Sunday said that the Attorney General has launched a review as to why there were no warning signs alerting people to the danger around the wildfires which would have allowed people to flee before the flames consumed the town.moving lines
