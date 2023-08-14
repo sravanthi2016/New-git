e7f46d52cd3dcb9b4856b1b6cb7a4259d0bd4f72
sravanthi <sravanthi.annam25@gmail.com>
2023-08-14 11:10:22 +0530

    changed

--- phone.sh
+++ phone.sh
@@ -1,3 +1,6 @@
-created script for testing git commands
+KULA, HAWAII - AUGUST 12: Brook Cretton holds up bowls that he found while looking through the rubble of a home that was destroyed by wildfire on August 12, 2023 in Kula, Hawaii. At least 80 people were killed and thousands were displaced after a wind-driven wildfire devastated the towns of Lahaina and Kula on Tuesday. Crews are continuing to search for missing people. Justin Sullivan/Getty Images/AFP (Photo by JUSTIN SULLIVAN / GETTY IMAGES NORTH AMERICA / Getty Images via AFP) (Getty Images via AFP)
+Hawaii wildfires are the deadliest wildfire in the US since the 2018 Camp Fire in California that killed at least 85 people and destroyed the town of Paradise. The fire went from zero to 100 and essentially managed to "eat an entire town in four hours".
+
+Here are 10 points you should know Hawaiicreated script for testing git commands
 practicing git commands
 removing lines
